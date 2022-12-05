5be47d7e598dbc71ad81d59730a85e7e79c46613
Tomasz Mikus <tmikus@twitch.tv>
2022-12-05 20:04:07 +0000

    Finish Day 5, part 1

--- day-05/main.go
+++ day-05/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,7 +15,20 @@ type Operation struct {
 }
 
 type Stack struct {
-	data []uint8
+	data []string
+}
+
+func move(fromStack Stack, toStack Stack, count int) (Stack, Stack) {
+	for i := 0; i < count; i++ {
+		fromStack, toStack = moveOne(fromStack, toStack)
+	}
+	return fromStack, toStack
+}
+
+func moveOne(fromStack Stack, toStack Stack) (Stack, Stack) {
+	toStack.data = append(toStack.data, fromStack.data[len(fromStack.data)-1])
+	fromStack.data = fromStack.data[:len(fromStack.data)-1]
+	return fromStack, toStack
 }
 
 func parseOperation(line string) Operation {
@@ -24,19 +38,19 @@ func parseOperation(line string) Operation {
 	to, _ := strconv.ParseInt(splitLine[5], 10, 32)
 	return Operation{
 		count: int(count),
-		from:  int(from),
-		to:    int(to),
+		from:  int(from - 1), // Convert to zero-based index
+		to:    int(to - 1),   // Convert to zero-based index
 	}
 }
 
 func parseStack(lines []string, stackIndex int) Stack {
-	data := make([]uint8, 0)
+	data := make([]string, 0)
 	sliceStartIndex := 4*stackIndex + 1
 	// Iterate in reverse to preserve the right order of data on the stack
 	for lineIndex := len(lines) - 1; lineIndex >= 0; lineIndex-- {
 		line := lines[lineIndex]
-		value := line[sliceStartIndex]
-		if value == ' ' {
+		value := string(line[sliceStartIndex])
+		if value == " " {
 			continue
 		}
 		data = append(data, value)
@@ -46,8 +60,8 @@ func parseStack(lines []string, stackIndex int) Stack {
 
 func parseStacks(lines []string) []Stack {
 	firstLine := lines[0]
-	stacksCount := len(firstLine) / 4
-	stacks := make([]Stack, stacksCount)
+	stacksCount := int(math.Ceil(float64(len(firstLine)) / 4.0))
+	stacks := make([]Stack, 0)
 	for stackIndex := 0; stackIndex < stacksCount; stackIndex++ {
 		stacks = append(stacks, parseStack(lines, stackIndex))
 	}
@@ -57,7 +71,7 @@ func parseStacks(lines []string) []Stack {
 func getResult(stacks []Stack) string {
 	result := ""
 	for _, stack := range stacks {
-		result += string(stack.data[len(stack.data)-1])
+		result += stack.data[len(stack.data)-1]
 	}
 	return result
 }
@@ -84,8 +98,19 @@ func readStacks(scanner *bufio.Scanner) []Stack {
 	return parseStacks(stackLinesWithoutLabels)
 }
 
-func runOperations(stacks []Stack, operations []Operation) []Stack {
+func runOperation(stacks []Stack, operation Operation) []Stack {
+	fromStack := stacks[operation.from]
+	toStack := stacks[operation.to]
+	fromStack, toStack = move(fromStack, toStack, operation.count)
+	stacks[operation.from] = fromStack
+	stacks[operation.to] = toStack
+	return stacks
+}
 
+func runOperations(stacks []Stack, operations []Operation) []Stack {
+	for _, operation := range operations {
+		stacks = runOperation(stacks, operation)
+	}
 	return stacks
 }
 
@@ -93,7 +118,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	stacks := readStacks(scanner)
 	operations := readOperations(scanner)
-	stacks := runOperations(stacks, operations)
-	println("Part 1 result:", stacks)
-	println("Part 2 result:", operations)
+	stacks = runOperations(stacks, operations)
+	output := getResult(stacks)
+	println("Part 1 result:", output)
 }
